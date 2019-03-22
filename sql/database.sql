c941c95f0538670e366641c16c1326954872bb38
Hycore Development <46926773+Hycore-Development@users.noreply.github.com>
2019-03-22 04:26:54 +0000

    Delete database.sql

--- sql/database.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE settings(
-    prefix VARCHAR(30),
-    verifyrole VARCHAR(30),
-    adminrole VARCHAR(30),
-    servername VARCHAR(30),
-    serverid VARCHAR(30),
-    serverowner VARCHAR(30)
-)
